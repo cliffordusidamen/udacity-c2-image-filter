e5408441a8dfc9e58dfe998710a7984f3b599389
Clifford Usidamen <cliffordusidamen@gmail.com>
2022-06-17 00:19:26 +0100

    Change the message sent when no image url is provided

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get('/filteredimage', async (req: Request, res: Response) => {
     const {image_url} = req.query;
     if (!image_url) {
-      return res.status(400).send('Image url is required');
+      return res.status(400).send('Please provide an image url');
     }
 
     const filteredpath = await filterImageFromURL(image_url);
